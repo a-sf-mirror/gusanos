55521e6b89eb4ca57a2ff170b45b375eaa1f6781
namename <namename>
2004-10-18 00:53:50 +0000

    more parser

--- Goop/text.cpp
+++ Goop/text.cpp
@@ -2,6 +2,12 @@
 
 using namespace std;
 
+
+/********************** separate_str_by ****************************************** 
+*  separates the string in 2 chunks the first chunk is from index 0 to the first *
+*  occurence of the given char, the second chunk is the rest of the src string   *
+*********************************************************************************/
+// should be changed to std::pair<string,string> acording to Gliptitc
 void separate_str_by(char ch, const string &src, string &left, string &right)
 {
 	int leftIndex = 0;
