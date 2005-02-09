89c16f94f07799ee4d9034abce7646761f9dc66d
nymacro <nymacro>
2005-02-09 02:09:49 +0000

    Updated Makefile.linux Added "FPS" text to framecounter in gusanos.cpp

--- Goop/gusanos.cpp
+++ Goop/gusanos.cpp
@@ -210,6 +210,7 @@ int main(int argc, char **argv)
                     stringstream sout;
                     sout << _fps;
                     sout >> fpsStr;
+                    fpsStr += " FPS";
                     font->draw(gfx.buffer, fpsStr, 5, 5, 0);
                 }
                 _fpsCount++;
