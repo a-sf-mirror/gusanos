47e474c951922f4024accf1461cad5b26047d3b9
nymacro <nymacro>
2005-02-24 10:39:18 +0000

    - Updated Makefile.linux to compile/link new source/objects. -

--- Goop/ai.cpp
+++ Goop/ai.cpp
@@ -8,6 +8,18 @@ inline float distance(float x1, float y1, float x2, float y2)
     return sqrt((x2 - x1)*(x2 - x1) + (y2 - y1)*(y2 - y1));
 }
 
+inline float todeg(float rad)
+{
+    return rad / (3.1416/180);
+}
+
+inline float torad(float rad)
+{
+    return rad * (3.1416/180);
+}
+
+const float cAimAccuracy = 8.0;
+
 void PlayerAI::getTarget()
 {
     //iterate through players
@@ -24,7 +36,8 @@ void PlayerAI::getTarget()
         int tmpY = tmpPos.y;
         if (abs(distance(x, y, tmpX, tmpY)) < tmpDist || tmpTarget == NULL)
         {
-            tmpTarget = *iter;
+            if (*iter != this)
+                tmpTarget = *iter;
         }
     }
     m_target = tmpTarget;
@@ -33,8 +46,12 @@ void PlayerAI::getTarget()
 void PlayerAI::think()
 {
     Player::think();
-    getTarget();
 
+    getTarget();
+    if (!m_target)
+        return;
+
+    //movement
     Vec pos = m_worm->getPos();
     int x = pos.x;
     int y = pos.y;
@@ -43,7 +60,8 @@ void PlayerAI::think()
     int tmpX = tmpPos.x;
     int tmpY = tmpPos.y;
 
-    if (abs(distance(x, y, tmpX, tmpY)) > 32)
+    int dist = abs(distance(x, y, tmpX, tmpY));
+    if (dist > 48)
     { 
         if (x < tmpX)
             actionStart(RIGHT);
@@ -53,10 +71,52 @@ void PlayerAI::think()
             actionStart(LEFT);
         else
             actionStop(LEFT);
+    } else if (dist < 24)
+    {
+        if (x > tmpX)
+            actionStart(RIGHT);
+        else
+            actionStop(RIGHT);
+        if (x < tmpX)
+            actionStart(LEFT);
+        else
+            actionStop(LEFT);
     } else
     {
         actionStop(LEFT);
         actionStop(RIGHT);
     }
+
+
+    //aiming
+    float curAngle = m_worm->getAngle() / m_worm->getDir();
+    float targetAngle = atan2(tmpY - y, tmpX - x);
+
+    //convert to gusanos angle system
+    targetAngle -= torad(90);
+    targetAngle = torad(180) + targetAngle;
+    if (m_worm->getDir() == -1)
+        targetAngle = torad(180) - (targetAngle - torad(180));
+
+    while (curAngle < 0)
+        curAngle += 360;
+    while (curAngle >= 360)
+        curAngle -= 360;
+
+    while (todeg(targetAngle) < 0)
+        targetAngle += torad(360);
+    while (todeg(targetAngle) >= 360)
+        targetAngle -= torad(360);
+
+    //aim
+    if (curAngle - cAimAccuracy > todeg(targetAngle))
+        actionStart(UP);
+    else
+        actionStop(UP);
+
+    if (curAngle + cAimAccuracy < todeg(targetAngle))
+        actionStart(DOWN);
+    else
+        actionStop(DOWN);
 }
 
