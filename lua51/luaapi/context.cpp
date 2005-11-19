f0414f1e007df6ed7c317e0e052d497216c20561
gliptic <gliptic>
2005-11-19 17:41:17 +0000

    Missed a directory

---
+++ lua51/luaapi/context.cpp
@@ -0,0 +1,488 @@
+#include "context.h"
+
+extern "C"
+{
+	#include "../lualib.h"
+}
+
+#include <iostream>
+using std::cerr;
+using std::endl;
+
+#define FREELIST_REF 1
+#define ARRAY_SIZE   2
+
+int LuaContext::errorReport(lua_State* L)
+{
+	LuaContext context(L);
+	
+	char const* s = lua_tostring(context, 1);
+	
+	lua_Debug info;
+	
+	cerr << "Lua error: " << s << ", call stack:" << endl;
+	for(int i = 1; lua_getstack(context, i, &info); ++i)
+	{
+		lua_getinfo (context, "Snl", &info);
+		
+		char const* name = info.name ? info.name : "N/A";
+		
+		cerr << i << ". " << name << " (" << info.namewhat << ") @ " << info.short_src << ":" << info.currentline << endl;
+	}
+	
+	context.pushvalue(1);
+	return 1;
+}
+
+LuaContext::LuaContext()
+: m_borrowed(false)
+{
+	init();
+}
+
+LuaContext::LuaContext(LuaContext const& b)
+: m_borrowed(true), m_State(b.m_State)
+{
+}
+
+LuaContext::LuaContext(lua_State* state_)
+: m_borrowed(true), m_State(state_)
+{
+
+}
+
+namespace
+{
+	void* l_alloc (void*, void* ptr, size_t, size_t nsize)
+	{
+		if (nsize == 0)
+		{
+			free(ptr);
+			return 0;
+		}
+		else
+			return realloc(ptr, nsize);
+	}
+}
+
+void LuaContext::init()
+{
+	//m_State = lua_open();
+	m_State = lua_newstate(l_alloc, 0);
+	lua_pushinteger(m_State, 3);
+	lua_rawseti(m_State, LUA_REGISTRYINDEX, ARRAY_SIZE);
+	
+	luaopen_base(m_State);
+	luaopen_table(m_State);
+	luaopen_string(m_State);
+	luaopen_math(m_State);
+}
+
+void LuaContext::reset()
+{
+	if(m_borrowed)
+		return;
+		
+	if(m_State)
+		lua_close(m_State);
+	init();
+}
+
+const char * LuaContext::istreamChunkReader(lua_State *L, void *data, size_t *size)
+{
+	static char buffer[1024];
+	
+	istream& stream = *(istream *)data;
+	
+	stream.read(buffer, 1024);
+
+	*size = (size_t)stream.gcount();
+	
+	if(*size > 0)
+		return buffer;
+	else
+		return 0;
+}
+
+void LuaContext::load(std::string const& chunk, istream& stream)
+{
+	lua_pushcfunction(m_State, errorReport);
+	int result = lua_load(m_State, istreamChunkReader, &stream, chunk.c_str());
+	
+	if(result)
+	{
+		cerr << "Lua error: " << lua_tostring(m_State, -1) << endl;
+		pop(1);
+		return;
+	}
+	
+	//TODO: Create an error handler function that returns additional
+	//debug info.
+	result = lua_pcall (m_State, 0, 0, -2);
+	
+	switch(result)
+	{
+		case LUA_ERRRUN:
+		case LUA_ERRMEM:
+		case LUA_ERRERR:
+		{
+			/*strcpy(errMsgSpace, lua_tostring(m_State, -1));
+			lua_pop(m_State, 1);*/
+			//TODO: throw lua_exception(*this);
+			//cerr << "Lua error: " << lua_tostring(m_State, -1) << endl;
+			pop(1);
+		}
+		break;
+	}
+	pop(1); // Pop error function
+}
+
+/*
+void LuaContext::load(std::string const& chunk, istream& stream, string const& table)
+{
+	lua_pushcfunction(m_State, errorReport);
+	int result = lua_load(m_State, istreamChunkReader, &stream, chunk.c_str());
+	
+	if(result)
+	{
+		cerr << "Lua error: " << lua_tostring(m_State, -1) << endl;
+		return;
+	}
+	
+	lua_pushstring(m_State, table.c_str());
+	lua_rawget(m_State, LUA_GLOBALSINDEX);
+	lua_setfenv(m_State, -2);
+	
+	//TODO: Create an error handler function that returns additional
+	//debug info.
+	result = lua_pcall (m_State, 0, 0, 0);
+	
+	switch(result)
+	{
+		case LUA_ERRRUN:
+		case LUA_ERRMEM:
+		case LUA_ERRERR:
+		{
+			//TODO: throw lua_exception(*this);
+			cerr << "Lua error: " << lua_tostring(m_State, -1) << endl;
+		}
+		break;
+	}
+}*/
+
+int LuaContext::call(int params, int returns, int errfunc)
+{
+	int result = lua_pcall (m_State, params, returns, errfunc);
+		
+	switch(result)
+	{
+		case LUA_ERRRUN:
+		case LUA_ERRMEM:
+		case LUA_ERRERR:
+		{
+			cerr << "Lua error: " << lua_tostring(m_State, -1) << endl;
+			pop(1);
+			return -1;
+		}
+		break;
+	}
+	
+	return returns;
+}
+
+/*
+int LuaContext::callReference(LuaReference ref)
+{
+	pushReference(ref);
+	int result = call(0, 0);
+	
+	return result;
+}*/
+
+void LuaContext::function(char const* name, lua_CFunction func, int upvalues)
+{
+	lua_pushcclosure(m_State, func, upvalues);
+	lua_setfield(m_State, LUA_GLOBALSINDEX, name);
+}
+
+void LuaContext::tableFunction(char const* name, lua_CFunction func)
+{
+	lua_pushstring(m_State, name);
+	lua_pushcfunction(m_State, func);
+	lua_rawset(m_State, -3);
+}
+
+LuaReference LuaContext::createReference()
+{
+	int ref;
+	int t = LUA_REGISTRYINDEX;
+	lua_rawgeti(m_State, t, FREELIST_REF);
+	//ref = (int)lua_tonumber(m_State, -1);
+	ref = static_cast<int>(lua_tointeger(m_State, -1));
+	lua_settop(m_State, -2);
+	if(ref != 0)
+	{
+		lua_rawgeti(m_State, t, ref);
+		lua_rawseti(m_State, t, FREELIST_REF);
+	}
+	else
+	{
+		lua_rawgeti(m_State, t, ARRAY_SIZE);
+		//ref = (int)lua_tonumber(m_State, -1);
+		ref = static_cast<int>(lua_tointeger(m_State, -1));
+		//lua_pushnumber(m_State, ref + 1);
+		lua_pushinteger(m_State, static_cast<lua_Integer>(ref + 1));
+		lua_rawseti(m_State, t, ARRAY_SIZE);
+		lua_settop(m_State, -2);
+	}
+	
+	lua_rawseti(m_State, t, ref);
+	return LuaReference(ref);
+}
+
+void LuaContext::destroyReference(LuaReference ref)
+{
+	if (ref.idx > 2)
+	{
+    	int t = LUA_REGISTRYINDEX;
+		lua_rawgeti(m_State, t, FREELIST_REF);
+		lua_rawseti(m_State, t, ref.idx);  /* t[ref] = t[FREELIST_REF] */
+		//lua_pushnumber(m_State, (lua_Number)ref.idx);
+		lua_pushinteger(m_State, static_cast<lua_Integer>(ref.idx));
+		lua_rawseti(m_State, t, FREELIST_REF);  /* t[FREELIST_REF] = ref */
+	}
+}
+
+void LuaContext::pushReference(LuaReference ref)
+{
+	lua_rawgeti(m_State, LUA_REGISTRYINDEX, ref.idx);
+}
+
+namespace LuaType
+{
+enum type
+{
+	Nil = 0,
+	Number,
+	BooleanFalse,
+	BooleanTrue,
+	String,
+	Table,
+	UserData,
+	End,
+};
+}
+
+void LuaContext::serialize(std::ostream& s, int i)
+{
+	switch(lua_type(m_State, i))
+	{
+		case LUA_TNIL:
+			s.put(LuaType::Nil);
+		break;
+		
+		case LUA_TNUMBER:
+		{
+			lua_Number n = lua_tonumber(m_State, i);
+			s.put(LuaType::Number);
+			s.write((char *)&n, sizeof(double));
+		}
+		break;
+		
+		case LUA_TBOOLEAN:
+		{
+			int n = lua_toboolean(m_State, i);
+			s.put(n ? LuaType::BooleanTrue : LuaType::BooleanFalse);
+		}
+		break;
+		
+		case LUA_TSTRING:
+		{
+			s.put(LuaType::String);
+			size_t len;
+			char const* n = lua_tolstring(m_State, i, &len);
+			s.put((len >> 24) & 0xFF);
+			s.put((len >> 16) & 0xFF);
+			s.put((len >> 8) & 0xFF);
+			s.put(len & 0xFF);
+			s.write(n, len);
+		}
+		break;
+		
+		case LUA_TTABLE:
+		{
+			s.put(LuaType::Table);
+			lua_pushnil(m_State);
+			int tab = i < 0 ? i - 1 : i;
+			while(lua_next(m_State, tab) != 0)
+			{
+				serialize(s, -2);
+				serialize(s, -1);
+				pop(1);
+			}
+			s.put(LuaType::End);
+		}
+		break;
+		
+		default: // Ignore any value we can't handle and encode a nil instead
+			s.put(LuaType::Nil);
+		break;
+	}
+}
+
+void LuaContext::deserialize(std::istream& s)
+{
+	int t = s.get();
+	switch(t)
+	{
+		case LuaType::Nil:
+			lua_pushnil(m_State);
+		break;
+		
+		case LuaType::Number:
+		{
+			lua_Number n;
+			s.read((char *)&n, sizeof(double));
+			push(n);
+		}
+		break;
+		
+		case LuaType::BooleanTrue:
+			push(true);
+		break;
+		
+		case LuaType::BooleanFalse:
+			push(false);
+		break;
+		
+		case LuaType::String:
+		{
+			int v1 = s.get();
+			int v2 = s.get();
+			int v3 = s.get();
+			int v4 = s.get();
+			size_t len = (v1 << 24) | (v2 << 16) | (v3 << 8) | v4;
+			char* str = new char[len];
+			try
+			{
+				s.read(str, len);
+				lua_pushlstring(m_State, str, len);				
+			}
+			catch(...)
+			{
+				delete[] str;
+				throw;
+			}
+			delete[] str;
+		}
+		break;
+		
+		case LuaType::Table:
+		{
+			lua_newtable(m_State);
+			while(s && s.peek() != LuaType::End)
+			{
+				deserialize(s);
+				deserialize(s);
+				lua_rawset(m_State, -3);
+			}
+			s.get(); // Skip end
+		}
+		break;
+	}
+}
+
+void LuaContext::serializeT(std::ostream& s, int i, int indent)
+{
+	switch(lua_type(m_State, i))
+	{
+		case LUA_TNIL:
+			s << "nil";
+		break;
+		
+		case LUA_TNUMBER:
+		{
+			lua_Number n = lua_tonumber(m_State, i);
+			s << n;
+		}
+		break;
+		
+		case LUA_TBOOLEAN:
+		{
+			int n = lua_toboolean(m_State, i);
+			s << (n ? "true" : "false");
+		}
+		break;
+		
+		case LUA_TSTRING:
+		{
+			char const* n = lua_tostring(m_State, i);
+			s << '"';
+			
+			for(char const* p = n; *p; ++p)
+			{
+				char c = *p;
+				if(c == '"')
+					s << "\\\"";
+				else
+					s << c;
+			}
+			s << '"';
+		}
+		break;
+		
+		case LUA_TTABLE:
+		{
+			s << "{\n";
+			++indent;
+			size_t idx = 1;
+			for(;; ++idx)
+			{
+				lua_rawgeti(m_State, i, idx);
+				if(lua_isnil(m_State, -1))
+				{
+					pop(1);
+					break;
+				}
+				
+				for(int j = 0; j < indent; ++j)
+					s.put('\t');
+				serializeT(s, -1);
+				s << ";\n";
+				pop(1);
+			}
+			
+			lua_pushnil(m_State);
+			int tab = i < 0 ? i - 1 : i;
+			while(lua_next(m_State, tab) != 0)
+			{
+				if(!lua_isnumber(m_State, -2) || lua_tointeger(m_State, -2) >= idx)
+				{
+					for(int j = 0; j < indent; ++j)
+						s.put('\t');
+					s << '[';
+					serializeT(s, -2, indent);
+					s << "] = ";
+					serializeT(s, -1, indent);
+					s << ";\n";
+				}
+				pop(1);
+			}
+			--indent;
+			for(int j = 0; j < indent; ++j)
+				s.put('\t');
+			s << '}';
+		}
+		break;
+		
+		default: // Ignore any value we can't handle and encode a nil instead
+			s << "nil";
+		break;
+	}
+}
+
+LuaContext::~LuaContext()
+{
+	if(m_State && !m_borrowed)
+		lua_close(m_State);
+}
---
+++ lua51/luaapi/context.h
@@ -0,0 +1,429 @@
+#ifndef LUA_CONTEXT_H
+#define LUA_CONTEXT_H
+
+extern "C"
+{
+#include "../lua.h"
+}
+
+#include "types.h"
+#include <iostream>
+#include <vector>
+#include <string>
+#include <istream>
+#include <ostream>
+#include <utility>
+#include <map>
+using std::istream;
+using std::cerr;
+using std::endl;
+using std::string;
+
+class LuaContext
+{
+public:
+	struct FunctionProxy
+	{
+		FunctionProxy(LuaContext& context)
+		: m_context(context)
+		{
+		}
+		
+		FunctionProxy const& operator()(char const* name, lua_CFunction func) const
+		{
+			m_context.function(name, func);
+			return *this;
+		}
+
+		LuaContext& m_context;
+	};
+	
+	struct TableFunctionProxy
+	{
+		TableFunctionProxy(LuaContext& context)
+		: m_context(context)
+		{
+		}
+		
+		TableFunctionProxy const& operator()(char const* name, lua_CFunction func) const
+		{
+			m_context.tableFunction(name, func);
+			return *this;
+		}
+
+		LuaContext& m_context;
+	};
+	
+	struct TableItemProxy
+	{
+		TableItemProxy(LuaContext& context)
+		: m_context(context)
+		{
+		}
+		
+		template<class T>
+		TableItemProxy const& operator()(char const* name, T const& v) const
+		{
+			lua_pushstring(m_context, name);
+			m_context.push(v);
+			lua_rawset(m_context, -3);
+			return *this;
+		}
+
+		LuaContext& m_context;
+	};
+	
+	struct CallProxy
+	{
+		CallProxy(LuaContext& context, LuaReference ref, int returns)
+		: m_context(context), m_ref(ref), m_params(0), m_returns(returns)
+		{
+			m_context.push(errorReport);
+			m_context.push(ref);
+		}
+		
+		template<class T>
+		CallProxy& operator,(T const& p)
+		{
+			++m_params;
+			m_context.push(p);
+			return *this;
+		}
+		
+		int operator()() const
+		{
+			if(lua_isnil(m_context, -m_params-1))
+			{
+				m_context.pop(m_params + 2);
+				return 0;
+			}
+			
+			int r = m_context.call(m_params, m_returns, -m_params-2);
+			
+			if(r < 0)
+			{
+				m_context.pop(1);
+				m_context.destroyReference(m_ref);
+				return 0;
+			}
+			lua_remove(m_context, -r-1);
+			return r;
+		}
+
+		LuaContext& m_context;
+		LuaReference m_ref;
+		int m_params;
+		int m_returns;
+	};
+	
+	static int errorReport(lua_State* L);
+	
+	LuaContext();
+	
+	LuaContext(LuaContext const&);
+	
+	LuaContext(lua_State* state_);
+	
+	void init();
+	
+	void reset();
+	
+	static const char * istreamChunkReader(lua_State *L, void *data, size_t *size);
+
+	void load(std::string const& chunk, istream& stream);
+	/*
+	void load(std::string const& chunk, istream& stream, string const& table);
+	*/
+	int call(int params = 0, int returns = 0, int errfunc = 0);
+	
+	LuaContext& push(lua_CFunction v)
+	{
+		lua_pushcfunction(m_State, v);
+		return *this;
+	}
+	
+	LuaContext& push(lua_Number v)
+	{
+		lua_pushnumber(m_State, v);
+		return *this;
+	}
+	
+	LuaContext& push(char const* v)
+	{
+		lua_pushstring(m_State, v);
+		return *this;
+	}
+	
+	LuaContext& push(std::string const& v)
+	{
+		push(v.c_str());
+		return *this;
+	}
+	
+	LuaContext& push(std::pair<char const*, char const*> v)
+	{
+		lua_pushlstring(m_State, v.first, v.second - v.first);
+		return *this;
+	}
+	
+	LuaContext& push(LuaReference v)
+	{
+		pushReference(v);
+		return *this;
+	}
+	
+	LuaContext& push(bool v)
+	{
+		lua_pushboolean(m_State, v);
+		return *this;
+	}
+	
+	LuaContext& push(int v)
+	{
+		lua_pushinteger(m_State, static_cast<lua_Integer>(v));
+		return *this;
+	}
+			
+	template<class T>
+	LuaContext& push(T* v)
+	{
+		v->pushLuaReference();
+		return *this;
+	}
+	
+	template<class T>
+	LuaContext& push(std::vector<T> const& v)
+	{
+		lua_newtable(m_State);
+		for(size_t n = 0; n < v.size(); ++n)
+		{
+			push(v[n]);
+			lua_rawseti(m_State, -2, n + 1);
+		}
+		return *this;
+	}
+	
+	template<class T>
+	void pushFullReference(T& x, LuaReference metatable)
+	{
+		T** i = (T **)lua_newuserdata (m_State, sizeof(T *));
+		*i = &x;
+		pushReference(metatable);
+		
+		lua_setmetatable(m_State, -2);
+	}
+	
+	template<class T>
+	void pushLightReference(T* x, LuaReference metatable)
+	{
+		lua_pushlightuserdata (m_State, x);
+		pushReference(metatable);
+		lua_setmetatable(m_State, -2);
+	}
+	
+	template<class T>
+	void pushFullReference(T& x)
+	{
+		T** i = (T **)lua_newuserdata (m_State, sizeof(T*));
+		*i = &x;
+	}
+	
+	template<class T>
+	void pushObject(T const& x)
+	{
+		T* i = (T *)lua_newuserdata (m_State, sizeof(T));
+		*i = x;
+	}
+	
+	/*
+	template<class T>
+	T* toObject(int idx)
+	{
+		if(!lua_setmetatable(m_State, idx))
+			lua_error(m_State, "Invalid argument");
+		push(T::metatable());
+		
+		T** p = lua_touserdata(m_State, idx);
+		if(!p) lua_error(m_State, "Invalid argument");
+		T* p2 = *p;
+	}*/
+	
+	void* pushObject(size_t count) // Pops a metatable from the stack
+	{
+		void* p = lua_newuserdata (m_State, count); // <metatable> <object>
+
+		lua_insert(m_State, -2); // <object> <metatable>
+		lua_setmetatable(m_State, -2);
+		
+		return p;
+	}
+	
+	void* pushObject(LuaReference metatable, size_t count)
+	{
+		void* p = lua_newuserdata (m_State, count);
+		
+		pushReference(metatable);
+		lua_setmetatable(m_State, -2);
+		
+		return p;
+	}
+	
+	LuaContext& pushvalue(int i)
+	{
+		lua_pushvalue(m_State, i);
+		return *this;
+	}
+	
+	LuaContext& rawgeti(int table, int key)
+	{
+		lua_rawgeti(m_State, table, key);
+		return *this;
+	}
+	
+	LuaContext& rawseti(int table, int key)
+	{
+		lua_rawseti(m_State, table, key);
+		return *this;
+	}
+	
+	char const* tostring(int i)
+	{
+		return lua_tostring(m_State, i);
+	}
+	
+	LuaContext& newtable()
+	{
+		lua_newtable(m_State);
+		return *this;
+	}
+	
+	LuaContext& pop(int c = 1)
+	{
+		lua_settop(m_State, (-1)-c);
+		return *this;
+	}
+	
+	CallProxy call(LuaReference ref, int returns = 0)
+	{
+		return CallProxy(*this, ref, returns);
+	}
+
+	template<class T>
+	inline T get(int idx)
+	{
+		return T();
+	}
+	
+	template<class T>
+	void tableToVector(std::vector<T> const& v)
+	{
+		v.clear();
+		for(size_t n = 1; ; ++n)
+		{
+			lua_rawgeti(m_State, -1, n + 1);
+			if(lua_isnil(m_State, -1))
+			{
+				lua_settop(m_State, -3); // Pop nil and table
+				return;
+			}
+			//TODO: v.push_back(get<T>(-1));
+			lua_settop(m_State, -2); // Pop value
+		}
+	}
+	
+	void function(char const* name, lua_CFunction func, int upvalues = 0);
+	
+	void tableFunction(char const* name, lua_CFunction func);
+	
+	FunctionProxy functions()
+	{
+		return FunctionProxy(*this);
+	}
+	
+	TableFunctionProxy tableFunctions()
+	{
+		return TableFunctionProxy(*this);
+	}
+	
+	TableItemProxy tableItems()
+	{
+		return TableItemProxy(*this);
+	}
+	
+	LuaReference createReference();
+	
+	void destroyReference(LuaReference ref);
+	
+	void pushReference(LuaReference ref);
+	
+	void serialize(std::ostream& s, int i);
+	void deserialize(std::istream& s);
+	void serializeT(std::ostream& s, int i, int indent = 0);
+	
+	void regObject(char const* name)
+	{
+		lua_setfield(m_State, LUA_REGISTRYINDEX, name);
+	}
+	
+	void regObjectKeep(char const* name)
+	{
+		lua_pushvalue(m_State, -1);
+		regObject(name);
+	}
+	
+	void pushRegObject(char const* name)
+	{
+		lua_getfield(m_State, LUA_REGISTRYINDEX, name);
+	}
+	
+	operator lua_State*() const
+	{
+		return m_State;
+	}
+	
+	~LuaContext();
+	
+private:
+	lua_State *m_State;
+	bool m_borrowed;
+	//std::map<std::string, LuaReference> metaTables;
+};
+
+template<>
+inline bool LuaContext::get<bool>(int idx)
+{
+	return lua_toboolean(m_State, idx);
+}
+
+#ifndef NDEBUG
+
+struct AssertStack
+{
+	AssertStack(LuaContext const& c_)
+	: c(c_)
+	{
+		stack = lua_gettop(c);
+	}
+	
+	~AssertStack()
+	{
+		int newStack = lua_gettop(c);
+		assert(stack == newStack);
+	}
+	
+	LuaContext const& c;
+	int stack;
+};
+
+#else
+
+struct AssertStack
+{
+	AssertStack(LuaContext const& c_)
+	{
+	}
+};
+
+#endif
+
+#endif //LUA_CONTEXT_H
